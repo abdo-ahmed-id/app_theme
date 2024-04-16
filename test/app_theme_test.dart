3b0f18b7105b6cd7f976aa99bf04f0537fc63246
abdo <a01226967080@gmail.com>
2024-04-16 15:12:25 +0200

    create example

--- test/app_theme_test.dart
+++ test/app_theme_test.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app_theme/app_theme.dart';
@@ -10,3 +11,4 @@ void main() {
     expect(calculator.addOne(0), 1);
   });
 }
+*/
